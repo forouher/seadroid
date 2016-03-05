a6b2eb1c57b335d5612dce49066b3a5141c104ff
Dariush Forouher <dariush@forouher.de>
2016-03-05 14:12:59 +0100

    revert

--- app/src/main/java/com/seafile/seadroid2/account/ui/AccountDetailActivity.java
+++ app/src/main/java/com/seafile/seadroid2/account/ui/AccountDetailActivity.java
@@ -1,6 +1,5 @@
 package com.seafile.seadroid2.account.ui;
 
-import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
@@ -45,7 +44,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.util.ArrayList;
 
-public class AccountDetailActivity extends Activity implements Toolbar.OnMenuItemClickListener {
+public class AccountDetailActivity extends BaseActivity implements Toolbar.OnMenuItemClickListener {
     private static final String DEBUG_TAG = "AccountDetailActivity";
 
     private static final String HTTP_PREFIX = "http://";
@@ -124,13 +123,11 @@ public class AccountDetailActivity extends Activity implements Toolbar.OnMenuIte
             int prefixLen = HTTP_PREFIX.length();
             serverText.setSelection(prefixLen, prefixLen);
         }
-        /*
         Toolbar toolbar = getActionBarToolbar();
         toolbar.setOnMenuItemClickListener(this);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(R.string.login);
-        */
         Log.d(DEBUG_TAG, "onCreate finished");
 
     }
@@ -384,7 +381,7 @@ public class AccountDetailActivity extends Activity implements Toolbar.OnMenuIte
                     }
                 });
                 Log.d(DEBUG_TAG, "LoginTask::onPostExecute showing dialog");
-                //dialog.show(getSupportFragmentManager(), SslConfirmDialog.FRAGMENT_TAG);
+                dialog.show(getSupportFragmentManager(), SslConfirmDialog.FRAGMENT_TAG);
                 return;
             }
 
